fd51268c99b0852d429fade7a33451d8724c4d46
Smell of curry <75345244+smell-of-curry@users.noreply.github.com>
2022-03-22 18:43:53 -0400

    removed beta plugins
    removed beta plugins

--- scripts/Smelly Api/plugins/index.js
+++ scripts/Smelly Api/plugins/index.js
@@ -3,10 +3,10 @@
 import "./_default/index.js";
 import "./Chat Ranks/index.js";
 
-import "./home/index.js";
-import "./Leaderboards/index.js";
-import "./Faction System/index.js";
-import "./Stackable Mobs/index.js";
+// import "./home/index.js";
+// import "./Leaderboards/index.js";
+// import "./Faction System/index.js";
+// import "./Stackable Mobs/index.js";
 
 // IN BETA RIGHT NOW:
 // import "./Anti Cheat/index.js";
